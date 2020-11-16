b94ae8c0fe57770a8d5a111003404ec9208b8d3d
CarlosSamuelGarcia216 <47200430+CarlosSamuelGarcia216@users.noreply.github.com>
2020-11-16 16:54:22 -0600

    Caambio2
    ya quedo

--- Compilador/Compilador-LenguajeJCR/Form1.Designer.cs
+++ Compilador/Compilador-LenguajeJCR/Form1.Designer.cs
@@ -30,126 +30,151 @@
         {
             this.rtxCodigoFuente = new System.Windows.Forms.RichTextBox();
             this.btnAnalizar = new System.Windows.Forms.Button();
-            this.lblText = new System.Windows.Forms.Label();
             this.rtxTokens = new System.Windows.Forms.RichTextBox();
             this.btnCargar = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.lsbSimbolos = new System.Windows.Forms.ListBox();
-            this.lblSimbolos = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.LineNumberTextBox = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // rtxCodigoFuente
             // 
-            this.rtxCodigoFuente.Location = new System.Drawing.Point(12, 15);
+            this.rtxCodigoFuente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.rtxCodigoFuente.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtxCodigoFuente.Font = new System.Drawing.Font("Tw Cen MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxCodigoFuente.ForeColor = System.Drawing.Color.Aqua;
+            this.rtxCodigoFuente.Location = new System.Drawing.Point(69, 0);
             this.rtxCodigoFuente.Name = "rtxCodigoFuente";
-            this.rtxCodigoFuente.Size = new System.Drawing.Size(523, 131);
+            this.rtxCodigoFuente.Size = new System.Drawing.Size(301, 384);
             this.rtxCodigoFuente.TabIndex = 0;
             this.rtxCodigoFuente.Text = "";
+            this.rtxCodigoFuente.SelectionChanged += new System.EventHandler(this.rtxCodigoFuente_SelectionChanged);
+            this.rtxCodigoFuente.VScroll += new System.EventHandler(this.rtxCodigoFuente_VScroll);
+            this.rtxCodigoFuente.TextChanged += new System.EventHandler(this.rtxCodigoFuente_TextChanged);
+            this.rtxCodigoFuente.KeyDown += new System.Windows.Forms.KeyEventHandler(this.rtxCodigoFuente_KeyDown);
             // 
             // btnAnalizar
             // 
-            this.btnAnalizar.Location = new System.Drawing.Point(641, 75);
+            this.btnAnalizar.Location = new System.Drawing.Point(131, 3);
             this.btnAnalizar.Name = "btnAnalizar";
-            this.btnAnalizar.Size = new System.Drawing.Size(77, 24);
+            this.btnAnalizar.Size = new System.Drawing.Size(82, 27);
             this.btnAnalizar.TabIndex = 1;
-            this.btnAnalizar.Text = "Analizar";
+            this.btnAnalizar.Text = "Iniciar";
             this.btnAnalizar.UseVisualStyleBackColor = true;
             this.btnAnalizar.Click += new System.EventHandler(this.btnAnalizar_Click);
             // 
-            // lblText
-            // 
-            this.lblText.AutoSize = true;
-            this.lblText.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblText.Location = new System.Drawing.Point(12, 191);
-            this.lblText.Name = "lblText";
-            this.lblText.Size = new System.Drawing.Size(89, 23);
-            this.lblText.TabIndex = 2;
-            this.lblText.Text = "TOKENS";
-            // 
             // rtxTokens
             // 
-            this.rtxTokens.Location = new System.Drawing.Point(12, 245);
+            this.rtxTokens.Location = new System.Drawing.Point(376, 0);
             this.rtxTokens.Name = "rtxTokens";
-            this.rtxTokens.Size = new System.Drawing.Size(523, 138);
+            this.rtxTokens.Size = new System.Drawing.Size(305, 385);
             this.rtxTokens.TabIndex = 3;
             this.rtxTokens.Text = "";
             // 
             // btnCargar
             // 
-            this.btnCargar.Location = new System.Drawing.Point(641, 15);
+            this.btnCargar.Location = new System.Drawing.Point(27, 3);
             this.btnCargar.Name = "btnCargar";
-            this.btnCargar.Size = new System.Drawing.Size(77, 41);
+            this.btnCargar.Size = new System.Drawing.Size(98, 26);
             this.btnCargar.TabIndex = 4;
             this.btnCargar.Text = "Cargar Archivo";
             this.btnCargar.UseVisualStyleBackColor = true;
             this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(588, 170);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 24);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
-            // 
             // lsbSimbolos
             // 
             this.lsbSimbolos.FormattingEnabled = true;
-            this.lsbSimbolos.Location = new System.Drawing.Point(608, 245);
+            this.lsbSimbolos.Location = new System.Drawing.Point(376, 1);
             this.lsbSimbolos.Name = "lsbSimbolos";
-            this.lsbSimbolos.Size = new System.Drawing.Size(171, 134);
+            this.lsbSimbolos.Size = new System.Drawing.Size(305, 95);
             this.lsbSimbolos.TabIndex = 6;
             // 
-            // lblSimbolos
+            // LineNumberTextBox
             // 
-            this.lblSimbolos.AutoSize = true;
-            this.lblSimbolos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSimbolos.Location = new System.Drawing.Point(604, 212);
-            this.lblSimbolos.Name = "lblSimbolos";
-            this.lblSimbolos.Size = new System.Drawing.Size(159, 20);
-            this.lblSimbolos.TabIndex = 7;
-            this.lblSimbolos.Text = "Tabla De Simbolos";
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(211, 178);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(143, 34);
-            this.richTextBox1.TabIndex = 8;
-            this.richTextBox1.Text = "";
+            this.LineNumberTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.LineNumberTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LineNumberTextBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LineNumberTextBox.Font = new System.Drawing.Font("Tw Cen MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LineNumberTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.LineNumberTextBox.Location = new System.Drawing.Point(0, 0);
+            this.LineNumberTextBox.Name = "LineNumberTextBox";
+            this.LineNumberTextBox.Size = new System.Drawing.Size(63, 385);
+            this.LineNumberTextBox.TabIndex = 8;
+            this.LineNumberTextBox.Text = "";
+            this.LineNumberTextBox.FontChanged += new System.EventHandler(this.LineNumberTextBox_FontChanged);
+            this.LineNumberTextBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LineNumberTextBox_MouseDown);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(382, 178);
+            this.button1.Location = new System.Drawing.Point(229, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 33);
+            this.button1.Size = new System.Drawing.Size(103, 27);
             this.button1.TabIndex = 9;
-            this.button1.Text = "button1";
+            this.button1.Text = "Guardar archivo";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.panel1.Controls.Add(this.btnCargar);
+            this.panel1.Controls.Add(this.btnAnalizar);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 33);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.rtxTokens);
+            this.panel2.Controls.Add(this.LineNumberTextBox);
+            this.panel2.Controls.Add(this.rtxCodigoFuente);
+            this.panel2.Location = new System.Drawing.Point(0, 39);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(799, 385);
+            this.panel2.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.richTextBox1);
+            this.panel3.Controls.Add(this.lsbSimbolos);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 430);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 96);
+            this.panel3.TabIndex = 12;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(351, 72);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
             // 
             // frmLJCR
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 483);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.lblSimbolos);
-            this.Controls.Add(this.lsbSimbolos);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnCargar);
-            this.Controls.Add(this.rtxTokens);
-            this.Controls.Add(this.lblText);
-            this.Controls.Add(this.btnAnalizar);
-            this.Controls.Add(this.rtxCodigoFuente);
+            this.ClientSize = new System.Drawing.Size(800, 526);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "frmLJCR";
             this.Text = "LENGUAJE JCR";
+            this.Resize += new System.EventHandler(this.frmLJCR_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -157,14 +182,15 @@
 
         private System.Windows.Forms.RichTextBox rtxCodigoFuente;
         private System.Windows.Forms.Button btnAnalizar;
-        private System.Windows.Forms.Label lblText;
         private System.Windows.Forms.RichTextBox rtxTokens;
         private System.Windows.Forms.Button btnCargar;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox lsbSimbolos;
-        private System.Windows.Forms.Label lblSimbolos;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox LineNumberTextBox;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
