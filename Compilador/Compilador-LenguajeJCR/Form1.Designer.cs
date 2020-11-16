8a714bcfc9b83411809aabb3aceacbc707d2036a
CarlosSamuelGarcia216 <47200430+CarlosSamuelGarcia216@users.noreply.github.com>
2020-11-16 15:32:29 -0600

    nuevo cambio
    solo prueba

--- Compilador/Compilador-LenguajeJCR/Form1.Designer.cs
+++ Compilador/Compilador-LenguajeJCR/Form1.Designer.cs
@@ -36,6 +36,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.lsbSimbolos = new System.Windows.Forms.ListBox();
             this.lblSimbolos = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // rtxCodigoFuente
@@ -88,7 +90,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(593, 160);
+            this.label1.Location = new System.Drawing.Point(588, 170);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(60, 24);
             this.label1.TabIndex = 5;
@@ -112,11 +114,30 @@
             this.lblSimbolos.TabIndex = 7;
             this.lblSimbolos.Text = "Tabla De Simbolos";
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(211, 178);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(143, 34);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 33);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmLJCR
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 483);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.lblSimbolos);
             this.Controls.Add(this.lsbSimbolos);
             this.Controls.Add(this.label1);
@@ -142,6 +163,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox lsbSimbolos;
         private System.Windows.Forms.Label lblSimbolos;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
